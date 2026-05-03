d63c0467a71594e02ff30f0f1b1d9faa46c057a9
LucasDroon <lucasdroon@gmail.com>
2026-05-02 20:05:43 -0400

    First Commit

---
+++ src/test/java/com/AgendeAi/Prod/ProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgendeAi.Prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
