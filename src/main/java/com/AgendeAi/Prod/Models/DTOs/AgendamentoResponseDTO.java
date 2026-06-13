c8a2c548e43d6ed64f40cef6345abf6e31f8a80f
LucasDroon <lucasdroon@gmail.com>
2026-06-12 20:52:51 -0400

    feat(DTOs): implement a DTO for response

--- src/main/java/com/AgendeAi/Prod/Models/DTOs/AgendamentoResponseDTO.java
+++ src/main/java/com/AgendeAi/Prod/Models/DTOs/AgendamentoResponseDTO.java
@@ -1,4 +1,31 @@
 package com.AgendeAi.Prod.Models.DTOs;
 
-public record AgendamentoResponseDTO() {
+import com.AgendeAi.Prod.Models.Entities.AgendamentosModel;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record AgendamentoResponseDTO(
+        Integer id,
+        String nomeCliente,
+        String nomeProfissional,
+        String nomeServico,
+        LocalDateTime dataHora,
+        String status,
+        BigDecimal valorPago
+
+) {
+
+    public AgendamentoResponseDTO toDTO(AgendamentosModel agendamento) {
+        return new AgendamentoResponseDTO(
+                agendamento.getId_agendamento(),
+                agendamento.getClientesModel().getNome(),
+                agendamento.getProfissionaisModel() != null ? agendamento.getProfissionaisModel().getNome() : "Não definido",
+                agendamento.getServicosModel().getNome(),
+                agendamento.getData_hora(),
+                agendamento.getStatus().name(),
+                agendamento.getValor_pago()
+        );
+    }
 }
+
