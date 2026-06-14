7fa6b1976d8c34ddba2c0775a4c30d109fafdea2
LucasDroon <lucasdroon@gmail.com>
2026-06-14 02:41:39 -0400

    feat(repository): implement a new function

--- src/main/java/com/AgendeAi/Prod/Repositories/UsuariosRepository.java
+++ src/main/java/com/AgendeAi/Prod/Repositories/UsuariosRepository.java
@@ -1,6 +1,8 @@
 package com.AgendeAi.Prod.Repositories;
 
 import com.AgendeAi.Prod.Models.Entities.UsuariosModel;
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,7 @@ import org.springframework.data.repository.query.Param;
 public interface UsuariosRepository extends JpaRepository<UsuariosModel, Integer> {
     @Query("SELECT u FROM UsuariosModel u JOIN FETCH u.perfisModel WHERE u.email = :email")
     Optional<UsuariosModel> findByEmail(@Param("email") String email);
+
+    @EntityGraph(attributePaths = {"perfisModel"})
+    List<UsuariosModel> findAll();
 }
